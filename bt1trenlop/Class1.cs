ac7eb92e324e77eaaf610ad9ebeda577ee951eac
ACER <ACER@LAPTOP-QHOF49SH>
2025-02-07 22:34:28 +0700

    Add project files.

---
+++ bt1trenlop/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace bt1trenlop
+{
+    public class kiemtraso
+    {
+        private int n;
+        public kiemtraso() { }
+        public void nhap(int x) { this.n = x; }
+        public void xuat() { Console.WriteLine(n);}
+    }
+}
---
+++ bt1trenlop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+namespace bt1trenlop
+{
+    public class Program
+    {
+    public static void Main(string[] args) { 
+        kiemtraso p1= new kiemtraso();
+            p1.nhap(10);
+            p1.xuat();
+    }
+    }
+}
